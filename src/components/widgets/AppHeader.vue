28c36845c34e68010d4e0353bdfd2e55d8e6d17b
Lars <29163322+Drumber@users.noreply.github.com>
2022-09-15 12:00:46 +0200

    Added background on scroll to AppHeader

--- src/components/widgets/AppHeader.vue
+++ src/components/widgets/AppHeader.vue
@@ -1,9 +1,25 @@
 <script setup>
 import ThemeSwitch from "./ThemeSwitch.vue";
+
+const vOnScroll = {
+  mounted: (el) => {
+    el.scrollListener = (event) => {
+      const scrollTop = event.target.documentElement.scrollTop;
+      el.classList.toggle("scrolled", scrollTop > 100);
+    };
+    window.addEventListener("scroll", el.scrollListener);
+  },
+  unmounted: (el) => {
+    window.removeEventListener("scroll", el.scrollListener);
+  },
+};
 </script>
 
 <template>
-  <header class="w-full sticky top-0 z-30 backdrop-blur-sm">
+  <header
+    v-on-scroll
+    class="w-full sticky top-0 z-30 transition-all duration-300"
+  >
     <div class="p-4 flex">
       <a href="/">KitsuStats</a>
       <div id="them-switch" class="ml-auto">
@@ -13,3 +29,9 @@ import ThemeSwitch from "./ThemeSwitch.vue";
     <slot></slot>
   </header>
 </template>
+
+<style scoped>
+.scrolled {
+  @apply bg-background-300/30 dark:bg-background-800/40 backdrop-blur-sm shadow-sm;
+}
+</style>
